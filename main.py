07118fc9fd596ef091106153c612cfd066054b0a
Hubert <hugraetz@live.com>
2021-10-05 23:26:33 +0200

    first init

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import alpaca_trade_api as alpaca
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+endpoint_url = "https://api.alpaca.markets"
 
+api = alpaca.REST("AKIZMY06T8MIP4ADKKOJ", "EHwINcWqlhZmEEnf0gRugRzqe8wUSsaDhHcPOkn7", endpoint_url)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+account = api.get_account()
+print(account.status)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
